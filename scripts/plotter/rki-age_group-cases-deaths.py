85a084982a0535f77f66d94cdd134cd63c0ff4d9
masdot <69101694+masdot@users.noreply.github.com>
2021-04-11 09:18:20 +0200

    Update rki-age_group-cases-deaths.py

--- scripts/plotter/rki-age_group-cases-deaths.py
+++ scripts/plotter/rki-age_group-cases-deaths.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 """
 NOTE: This code is part of CDP (Corona-Data-Proejct) developed by @masdot. 
 Copyright (c) 2021 masdot - Licensed under MIT License (https://github.com/masdot/cdp-corona-data-project/blob/main/LICENSE)
